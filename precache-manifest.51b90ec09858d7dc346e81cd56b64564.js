04026ba8edfd8ef1161bb16bd5294170fa539db8
Vindyashree S <Vindyashree@cloudthing.com>
2019-12-20 12:37:08 +0530

    Updates

---
+++ precache-manifest.51b90ec09858d7dc346e81cd56b64564.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76425ce1d7404c21a7df1519b0742143",
+    "url": "/Bar-Chart/index.html"
+  },
+  {
+    "revision": "03bf95cba127f3858c15",
+    "url": "/Bar-Chart/static/css/main.951face8.chunk.css"
+  },
+  {
+    "revision": "fe6ecda65be238924b4b",
+    "url": "/Bar-Chart/static/js/2.1cca991b.chunk.js"
+  },
+  {
+    "revision": "57c459976375f0b6fa1cf7f6070c2a0d",
+    "url": "/Bar-Chart/static/js/2.1cca991b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03bf95cba127f3858c15",
+    "url": "/Bar-Chart/static/js/main.5e6e5eca.chunk.js"
+  },
+  {
+    "revision": "4962bb07630aa5340eb8",
+    "url": "/Bar-Chart/static/js/runtime-main.414f3146.js"
+  }
+]);
